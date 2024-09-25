942bf100665817fe5e210f04bc2ec2621f5e3a73
gauribehl100 <140520216+gauribehl100@users.noreply.github.com>
2024-09-25 16:33:41 +0530

    Banner.jsx

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -37,7 +37,7 @@ const BannerComponent = () => {
               fontFamily: 'Montserrat',
             }}
           >
-            Crypto Hunter
+            Crypto Chaser
           </Typography>
           <Typography
             variant="subtitle2"
